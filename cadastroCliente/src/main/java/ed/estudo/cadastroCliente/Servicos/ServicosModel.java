21003d2720decc7f10b8e6749d175d27be042e72
Edgar Campetelli <edcamprosa2@gmail.com>
2026-05-13 12:42:47 -0300

    Add lombok to @NoArgsConstructor

--- cadastroCliente/src/main/java/ed/estudo/cadastroCliente/Servicos/ServicosModel.java
+++ cadastroCliente/src/main/java/ed/estudo/cadastroCliente/Servicos/ServicosModel.java
@@ -2,42 +2,27 @@ package ed.estudo.cadastroCliente.Servicos;
 
 import ed.estudo.cadastroCliente.Clientes.ClienteModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_servicos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data // Getter an Setters
 public class ServicosModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String dificuldade;
+
     @OneToMany(mappedBy = "servicos") // Um servico para MUITOS clientes
     private List<ClienteModel> clientes ;
 
-    public ServicosModel() {
-    }
 
-    public ServicosModel(Long id, String nome, String dificuldade) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 }
