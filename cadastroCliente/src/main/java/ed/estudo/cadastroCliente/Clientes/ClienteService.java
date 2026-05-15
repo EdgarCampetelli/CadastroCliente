d5ae49b150551c89e7a2cac3f58b16197f50be7d
Edgar Campetelli <edcamprosa2@gmail.com>
2026-05-15 15:59:32 -0300

    Feat:

---
+++ cadastroCliente/src/main/java/ed/estudo/cadastroCliente/Clientes/ClienteService.java
@@ -0,0 +1,4 @@
+package ed.estudo.cadastroCliente.Clientes;
+
+public class ClienteService {
+}
---
+++ cadastroCliente/src/main/java/ed/estudo/cadastroCliente/Servicos/ServicosRepository.java
@@ -0,0 +1,7 @@
+package ed.estudo.cadastroCliente.Servicos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicosRepository extends JpaRepository<ServicosModel, Long> {
+
+}
