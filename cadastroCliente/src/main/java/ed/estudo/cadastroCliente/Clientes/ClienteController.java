c746a24db9266c45b6013c10110e0aacbd896705
Edgar Campetelli Rosa <edcamprosa2@gmail.com>
2026-05-16 20:05:03 -0300

    Fix: Modified application.properties and controllers

--- cadastroCliente/src/main/java/ed/estudo/cadastroCliente/Clientes/ClienteController.java
+++ cadastroCliente/src/main/java/ed/estudo/cadastroCliente/Clientes/ClienteController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-
 public class ClienteController {
 
     @GetMapping("/boasvindas")
--- cadastroCliente/src/main/java/ed/estudo/cadastroCliente/Servicos/ServicosController.java
+++ cadastroCliente/src/main/java/ed/estudo/cadastroCliente/Servicos/ServicosController.java
@@ -1,4 +1,13 @@
 package ed.estudo.cadastroCliente.Servicos;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class ServicosController {
+
+    @GetMapping("/default")
+    public String linkDefault(){ return "Page Default"; }
 }
