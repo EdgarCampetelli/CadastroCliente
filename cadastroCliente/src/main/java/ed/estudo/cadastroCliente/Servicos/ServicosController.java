84ac67c2099db1d7aaa29ee142eb9291c638256d
Edgar Campetelli <edcamprosa2@gmail.com>
2026-05-19 12:20:31 -0300

    Feat: Creat ServicosController routes with mock data.

--- cadastroCliente/src/main/java/ed/estudo/cadastroCliente/Servicos/ServicosController.java
+++ cadastroCliente/src/main/java/ed/estudo/cadastroCliente/Servicos/ServicosController.java
@@ -1,13 +1,30 @@
 package ed.estudo.cadastroCliente.Servicos;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("servicos")
 public class ServicosController {
 
     @GetMapping("/default")
     public String linkDefault(){ return "Page Default"; }
+
+    @PostMapping("/creat")
+    public String creatServico(){return "Servico Criado";}
+
+    //READ Servico
+    @GetMapping("/readAll")
+    public String readAllServico(){return "Mostrar TODOS os Servico";}
+
+    //UPDATE Servico
+    @PutMapping("/updateID")
+    public String updateServicoID(){return "Altera Servico por ID";}
+
+    //DELETE Servico
+    @DeleteMapping("/deleteID")
+    public String deleteServicoID(){return "Deleta Servico por ID";}
+
+    //SEARCH Servico
+    @GetMapping("/readID")
+    public String readServicoID(){return "Mostrar Servico por ID";}
 }
